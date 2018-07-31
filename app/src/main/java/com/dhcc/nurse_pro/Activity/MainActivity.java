42266ada2ce16f7dfc8b68a05f4a6210862cc557
_Q <1083100489@qq.com>
2018-07-31 15:38:43 +0800

    更改包名

--- app/src/main/java/com/dhcc/nurse_pro/Activity/MainActivity.java
+++ app/src/main/java/com/dhcc/nurse_pro/Activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.nurse_pro;
+package com.dhcc.nurse_pro.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.dhcc.nurse_pro.R;
 
 public class MainActivity extends AppCompatActivity {
 
