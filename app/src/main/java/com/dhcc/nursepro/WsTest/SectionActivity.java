aad61a5b77914cb9d28da76012284ee0c503f757
devlix <434973681@qq.com>
2018-08-08 18:13:09 +0800

    筛选列表测试页

---
+++ app/src/main/java/com/dhcc/nursepro/WsTest/SectionActivity.java
@@ -0,0 +1,201 @@
+package com.dhcc.nursepro.WsTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.dhcc.nursepro.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters;
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
+import io.github.luizgrp.sectionedrecyclerviewadapter.StatelessSection;
+
+public class SectionActivity extends AppCompatActivity {
+
+    private RecyclerView sectionRecyleft;
+    private RecyclerView sectionRecyright;
+
+    private SectionedRecyclerViewAdapter sectionAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section);
+
+        initView();
+        initAdapter();
+        initData();
+    }
+
+    private void initView() {
+        sectionRecyleft = findViewById(R.id.section_recyleft);
+        //提高展示效率
+        sectionRecyleft.setHasFixedSize(true);
+        //设置的布局管理
+        sectionRecyleft.setLayoutManager(new LinearLayoutManager(this));
+        sectionRecyright = findViewById(R.id.section_recyright);
+        //提高展示效率
+        sectionRecyright.setHasFixedSize(true);
+        //设置的布局管理
+        sectionRecyright.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+    private void initAdapter() {
+        sectionAdapter = new SectionedRecyclerViewAdapter();
+
+        sectionAdapter.addSection(new NewsSection(NewsSection.PATIENT));
+        sectionAdapter.addSection(new NewsSection(NewsSection.RIORITY));
+        sectionAdapter.addSection(new NewsSection(NewsSection.STATUS));
+        sectionAdapter.addSection(new NewsSection(NewsSection.CATEGORY));
+        sectionRecyleft.setAdapter(sectionAdapter);
+    }
+
+    private void initData() {
+
+    }
+
+    public class NewsSection extends StatelessSection {
+
+        final static int PATIENT = 0;
+        final static int RIORITY = 1;
+        final static int STATUS = 2;
+        final static int CATEGORY = 3;
+
+        final int topic;
+
+        String title;
+        List<String> list;
+
+        NewsSection(int topic) {
+            super(SectionParameters.builder()
+                    .itemResourceId(R.layout.item_section)
+                    .headerResourceId(R.layout.item_section_header)
+                    //                    .footerResourceId(R.layout.section_ex2_footer)
+                    .build());
+
+            this.topic = topic;
+
+            switch (topic) {
+                case PATIENT:
+                    this.title = "病人列表";
+                    this.list = getNews(R.array.patient_list);
+                    break;
+                case RIORITY:
+                    this.title = "医嘱优先级";
+                    this.list = getNews(R.array.medical_order_priority);
+                    break;
+                case STATUS:
+                    this.title = "医嘱状态";
+                    this.list = getNews(R.array.medical_order_status);
+                    break;
+                case CATEGORY:
+                    this.title = "医嘱类别";
+                    this.list = getNews(R.array.medical_order_category);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private List<String> getNews(int arrayResource) {
+            return new ArrayList<>(Arrays.asList(getResources().getStringArray(arrayResource)));
+        }
+
+        @Override
+        public int getContentItemsTotal() {
+            return list.size();
+        }
+
+        @Override
+        public RecyclerView.ViewHolder getItemViewHolder(View view) {
+            return new ItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindItemViewHolder(RecyclerView.ViewHolder holder, int position) {
+            final ItemViewHolder itemHolder = (ItemViewHolder) holder;
+
+
+            final String content = list.get(position);
+            itemHolder.tvSection.setText(content);
+
+            itemHolder.rootView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    Toast.makeText(SectionActivity.this,
+//                            String.format("Clicked on position #%s of Section %s",
+//                                    sectionAdapter.getPositionInSection(itemHolder.getAdapterPosition()),
+//                                    title),
+//                            Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SectionActivity.this, title+"---"+content, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public RecyclerView.ViewHolder getHeaderViewHolder(View view) {
+            return new HeaderViewHolder(view);
+        }
+
+        @Override
+        public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder) {
+            HeaderViewHolder headerHolder = (HeaderViewHolder) holder;
+
+            headerHolder.tvSectionTitle.setText(title);
+        }
+
+//        @Override
+//        public RecyclerView.ViewHolder getFooterViewHolder(View view) {
+//            return new FooterViewHolder(view);
+//        }
+
+//        @Override
+//        public void onBindFooterViewHolder(RecyclerView.ViewHolder holder) {
+//            FooterViewHolder footerHolder = (FooterViewHolder) holder;
+//
+//            footerHolder.rootView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Toast.makeText(getContext(), String.format("Clicked on footer of Section %s", title), Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        }
+
+
+    }
+
+    private class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView tvSectionTitle;
+
+        HeaderViewHolder(View view) {
+            super(view);
+
+            tvSectionTitle = (TextView) view.findViewById(R.id.itemsectionheader_title);
+        }
+    }
+
+
+    private class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final View rootView;
+        private final TextView tvSection;
+
+        ItemViewHolder(View view) {
+            super(view);
+
+            rootView = view;
+            tvSection = (TextView) view.findViewById(R.id.itemsection_textview);
+        }
+    }
+}
--- app/src/main/java/com/dhcc/nursepro/setting/SettingFragment.java
+++ app/src/main/java/com/dhcc/nursepro/setting/SettingFragment.java
@@ -1,16 +1,25 @@
 package com.dhcc.nursepro.setting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dhcc.nursepro.BaseActivity;
 import com.dhcc.nursepro.BaseFragment;
 import com.dhcc.nursepro.R;
+import com.dhcc.nursepro.WsTest.ClassificationActivity;
+import com.dhcc.nursepro.WsTest.SectionActivity;
 
 public class SettingFragment extends BaseFragment {
+    private TextView tvSettingClassification;
+    private TextView tvSettingSection;
+
+
+
     @Override
     public View onCreateViewByYM(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_setting,container,false);
@@ -37,6 +46,23 @@ public class SettingFragment extends BaseFragment {
 
 
     private void initView(View view){
-
+        tvSettingClassification = getActivity().findViewById(R.id.setting_classification);
+        tvSettingClassification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentClassification = new Intent(getActivity(), ClassificationActivity.class);
+                intentClassification.putExtra("settingId",1);
+                startActivity(intentClassification);
+            }
+        });
+        tvSettingSection = getActivity().findViewById(R.id.setting_section);
+        tvSettingSection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSection = new Intent(getActivity(), SectionActivity.class);
+                intentSection.putExtra("settingId",2);
+                startActivity(intentSection);
+            }
+        });
     }
 }
