7be458d35f8496539db7a4b8a4f30b43e39aacfa
qiusibao <1083100489@qq.com>
2018-08-31 16:24:17 +0800

    医嘱查询

--- app/src/main/java/com/dhcc/nursepro/Activity/NurseSetActivity.java
+++ app/src/main/java/com/dhcc/nursepro/Activity/NurseSetActivity.java
@@ -54,7 +54,7 @@ public class NurseSetActivity extends AppCompatActivity implements View.OnClickL
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getSupportActionBar().hide();
+
         setContentView(R.layout.activity_nurse_set);
 
         initview();
