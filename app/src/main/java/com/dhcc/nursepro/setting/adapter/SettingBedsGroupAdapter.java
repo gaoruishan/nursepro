bbdd7b5cc4bc489c718fd7d4c27d8151b192e983
devlix <434973681@qq.com>
2019-01-23 17:05:20 +0800

    床位选择奔溃，添加判断

--- app/src/main/java/com/dhcc/nursepro/setting/adapter/SettingBedsGroupAdapter.java
+++ app/src/main/java/com/dhcc/nursepro/setting/adapter/SettingBedsGroupAdapter.java
@@ -1,12 +1,10 @@
 package com.dhcc.nursepro.setting.adapter;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -82,7 +80,12 @@ public class SettingBedsGroupAdapter extends BaseQuickAdapter<SettingBedListBean
         /**
          * 判断recyclerview是否存在ItemDecoration,若存在则不必重复添加
          */
-        RecyclerView.ItemDecoration itemDecoration = rectBedSelectBed.getItemDecorationAt(0);
+        RecyclerView.ItemDecoration itemDecoration;
+        if (rectBedSelectBed.getItemDecorationCount() == 0) {
+            itemDecoration = null;
+        } else {
+            itemDecoration = rectBedSelectBed.getItemDecorationAt(0);
+        }
         if (itemDecoration == null) {
             int spanCount = 3;
             int spacing = 20;
--- app/src/main/java/com/dhcc/nursepro/workarea/bedselect/adapter/BedGroupListAdapter.java
+++ app/src/main/java/com/dhcc/nursepro/workarea/bedselect/adapter/BedGroupListAdapter.java
@@ -3,7 +3,6 @@ package com.dhcc.nursepro.workarea.bedselect.adapter;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -56,7 +55,12 @@ public class BedGroupListAdapter extends BaseQuickAdapter<BedSelectListBean.BedL
         /**
          * 判断recyclerview是否存在ItemDecoration,若存在则不必重复添加
          */
-        RecyclerView.ItemDecoration itemDecoration = rectBedSelectBed.getItemDecorationAt(0);
+        RecyclerView.ItemDecoration itemDecoration;
+        if (rectBedSelectBed.getItemDecorationCount() == 0) {
+            itemDecoration = null;
+        } else {
+            itemDecoration = rectBedSelectBed.getItemDecorationAt(0);
+        }
         if (itemDecoration == null) {
             int spanCount = 3;
             int spacing = 20;
