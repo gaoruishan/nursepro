73868f66e498de7c90779e2f5c15f524aaf324f3
devlix <434973681@qq.com>
2019-07-23 17:43:04 +0800

    检验打包标本状态

--- app/src/main/java/com/dhcc/nursepro/workarea/labout/adapter/LabOutDetailAdapter.java
+++ app/src/main/java/com/dhcc/nursepro/workarea/labout/adapter/LabOutDetailAdapter.java
@@ -1,9 +1,6 @@
 package com.dhcc.nursepro.workarea.labout.adapter;
 
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -12,7 +9,6 @@ import com.dhcc.nursepro.R;
 import com.dhcc.nursepro.workarea.labout.bean.LabOutDetailBean;
 
 import java.util.List;
-import java.util.Map;
 
 public class LabOutDetailAdapter extends BaseQuickAdapter<LabOutDetailBean.DetailListBean, BaseViewHolder>{
 
@@ -26,17 +22,17 @@ public class LabOutDetailAdapter extends BaseQuickAdapter<LabOutDetailBean.Detai
         helper.setText(R.id.tv_laboutdetail_patinfo,item.getBedNo()+"  "+item.getPatName())
                 .setText(R.id.tv_laboutdetail_no,item.getCarryNo())
                 .setText(R.id.tv_laboutdetail_desc,item.getCarryLabDesc())
-                .setText(R.id.tv_laboutdetail_status,item.getStatusDesc())
+                .setText(R.id.tv_laboutdetail_status,item.getTranStatus())
                 .setText(R.id.tv_labout_specname,item.getSpecName())
                 .addOnClickListener(R.id.messagerightmenu);
 
+        //生成:P,采集:C,发送:S,台收:T,岗收:R,审定:V,查看:L,打印:U,退还:D,拒绝:F
+
         TextView tvStatus = helper.getView(R.id.tv_laboutdetail_status);
-        if (item.getStatusDesc().equals("建单")){
-            tvStatus.setBackgroundResource(R.drawable.bg_labout_tv1);
-        }else if (item.getStatusDesc().equals("已交接")){
+        if (item.getTranStatus().equals("退还")){
             tvStatus.setBackgroundResource(R.drawable.bg_labout_tv2);
-        }else if (item.getStatusDesc().equals("部分处理")){
-            tvStatus.setBackgroundResource(R.drawable.bg_labout_tv3);
+        }else if (item.getTranStatus().equals("拒绝")){
+            tvStatus.setBackgroundResource(R.drawable.bg_labout_tv5);
         }else{
             tvStatus.setBackgroundResource(R.drawable.bg_labout_tv4);
         }
