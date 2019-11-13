b0b23a2477f05078f5a3448562082d305df6b026
高瑞山 <grs0515@163.com>
2019-11-13 19:31:29 +0800

    门诊输液-添加采血模块

--- libraries/dhcc_infusion/src/main/java/com/dhcc/module/infusion/workarea/blood/BloodCollectionAdapter.java
+++ libraries/dhcc_infusion/src/main/java/com/dhcc/module/infusion/workarea/blood/BloodCollectionAdapter.java
@@ -3,9 +3,11 @@ package com.dhcc.module.infusion.workarea.blood;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.widget.TextView;
 
+import com.base.commlibs.utils.RecyclerViewHelper;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.dhcc.module.infusion.R;
@@ -29,6 +31,7 @@ public class BloodCollectionAdapter extends BaseQuickAdapter<BloodCollectBean.Or
         helper.setText(R.id.tv_lab_no, item.getLabNo())
                 .setText(R.id.tv_dose, item.getDoseQtyUnit())
                 .setText(R.id.tv_creator, item.getCtcpDesc())
+                .setText(R.id.tv_operate,item.getSpecDesc())
                 .setText(R.id.tv_time, item.getCreateDateTime());
         TextView blTvStatus = helper.getView(R.id.bl_tv_status);
         String labColor = item.getLabColor();
@@ -41,5 +44,22 @@ public class BloodCollectionAdapter extends BaseQuickAdapter<BloodCollectBean.Or
                 .build();
         blTvStatus.setBackground(drawable);
         helper.setGone(R.id.tv_time, !TextUtils.isEmpty(item.getCreateDateTime()));
+
+        RecyclerView rvItem = helper.getView(R.id.rv_item);
+        RecyclerViewHelper.setGridRecyclerView(mContext, rvItem, 2, 0, false);
+        BloodCollectionChildAdapter childAdapter = new BloodCollectionChildAdapter(item.getArcimDescList());
+        rvItem.setAdapter(childAdapter);
+    }
+
+    private class BloodCollectionChildAdapter extends BaseQuickAdapter<BloodCollectBean.OrdListBean.ArcimDescListBean,BaseViewHolder> {
+
+        public BloodCollectionChildAdapter( @Nullable List<BloodCollectBean.OrdListBean.ArcimDescListBean> data) {
+            super(R.layout.item_blood_child_layout, data);
+        }
+
+        @Override
+        protected void convert(BaseViewHolder helper, BloodCollectBean.OrdListBean.ArcimDescListBean item) {
+            helper.setText(R.id.tv_name, item.getArcimDesc());
+        }
     }
 }
