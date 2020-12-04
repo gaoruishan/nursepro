bb7dae218db66e5abc7a77cce4e7a13fc95566bc
高瑞山 <grs0515@163.com>
2020-12-04 17:15:53 +0800

    修改API

---
+++ libraries/dhcc_infusion/src/main/java/com/dhcc/module/infusion/ServerAPI.java
@@ -0,0 +1,14 @@
+package com.dhcc.module.infusion;
+
+/**
+ * API接口
+ * @author:gaoruishan
+ * @date:202020/12/4/16:37
+ * @email:grs0515@163.com
+ */
+public  class ServerAPI {
+
+    public static final String LOGON = "Logon";
+
+
+}
--- libraries/dhcc_infusion/src/main/java/com/dhcc/module/infusion/login/api/LoginApiService.java
+++ libraries/dhcc_infusion/src/main/java/com/dhcc/module/infusion/login/api/LoginApiService.java
@@ -4,6 +4,7 @@ import com.base.commlibs.constant.SharedPreference;
 import com.base.commlibs.http.CommWebService;
 import com.base.commlibs.http.ServiceCallBack;
 import com.blankj.utilcode.util.SPUtils;
+import com.dhcc.module.infusion.ServerAPI;
 
 import java.util.HashMap;
 
@@ -12,7 +13,8 @@ import java.util.HashMap;
  * @author DevLix126
  * @date 2018/8/13
  */
-public class LoginApiService {
+public class LoginApiService extends ServerAPI {
+
     public static void getLogin(String userCode, String password, String logonWardId, String scanFlag, final ServiceCallBack callback) {
         HashMap<String, String> properties = new HashMap<String, String>();
         properties.put("userCode", userCode);
@@ -20,7 +22,7 @@ public class LoginApiService {
         properties.put("logonWardId", logonWardId);
         properties.put("scanFlag", scanFlag);
         properties.put("logonLocType", SPUtils.getInstance().getString(SharedPreference.LOGONLOCTYPE));
-        CommWebService.call("Logon", properties, callback);
+        CommWebService.call(LOGON, properties, callback);
     }
 
     public static void getBroadcastList(ServiceCallBack callBack) {
