d7a0da59d8b07f03f972b656cfbb1580c7ca4bad
qiusibao <1083100489@qq.com>
2021-06-08 09:27:54 +0800

    床位图界面优化

--- app/src/main/java/com/dhcc/nursepro/view/CareLevelTextView.java
+++ app/src/main/java/com/dhcc/nursepro/view/CareLevelTextView.java
@@ -3,6 +3,7 @@ package com.dhcc.nursepro.view;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,20 +33,35 @@ public class CareLevelTextView extends android.support.v7.widget.AppCompatTextVi
 
     @Override
     public void setText(CharSequence text, BufferType type) {
-        super.setText(text, type);
+        if (text.length()>0){
+            super.setText(text.subSequence(0,1), type);
+        }else {
+            super.setText(text, type);
+        }
+        setPadding(20,10,20,10);
+        GradientDrawable drawable = (GradientDrawable) getResources().getDrawable(R.drawable.bg_carelevel1);
         String careLevelDesc = text.toString();
         if ("".equals(careLevelDesc)) {
             setVisibility(View.GONE);
         } else {
             setVisibility(View.VISIBLE);
+            setTextColor(Color.WHITE);
             if (careLevelDesc.contains("特级")) {
-                setTextColor(Color.parseColor("#FF8C00"));
+//                setTextColor(Color.parseColor("#FF8C00"));
+                drawable.setColor(getResources().getColor(R.color.dhcc_red));
+                setBackground(drawable);
             } else if(careLevelDesc.contains("一级")){
-                setTextColor(Color.parseColor("#FF0000"));
+                drawable.setColor(getResources().getColor(R.color.dhcc_lightpink));
+//                setTextColor(Color.parseColor("#FF0000"));
+                setBackground(getResources().getDrawable(R.drawable.bg_carelevel1));
             } else if (careLevelDesc.contains("二级")) {
-                setTextColor(Color.parseColor("#0000FF"));
+//                setTextColor(Color.parseColor("#0000FF"));
+                drawable.setColor(getResources().getColor(R.color.blue));
+                setBackground(getResources().getDrawable(R.drawable.bg_carelevel1));
             } else {
-                setTextColor(Color.parseColor("#00BD4C"));
+//                setTextColor(Color.parseColor("#00BD4C"));
+                drawable.setColor(getResources().getColor(R.color.bg_green));
+                setBackground(getResources().getDrawable(R.drawable.bg_carelevel1));
             }
         }
     }
