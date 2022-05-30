8b635958bc52d79f53874821dfc41aa8ad8ed1bb
高瑞山 <grs0515@163.com>
2022-05-30 09:59:56 +0800

    删除其他

--- dhcc_res/readme.txt
+++
@@ -1,5 +0,0 @@
-
-说明:
-1,drawable-nodpi:  放置图片资源
-2,com.dhcc.res: 存放自定义View(nurse护士站,infusion门诊输液)
-3,资源命名,建议加前缀dhcc
--- dhcc_res/src/main/java/com/base/commlibs/utils/SimpleCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.base.commlibs.utils;
-
-/**
- * 回调接口
- * @param <T>
- */
-public interface SimpleCallBack<T> {
-    void call(T result, int type);
-}
